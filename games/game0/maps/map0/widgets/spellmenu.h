7a665d0273e053b75bde384079f915b15791c2b9
Boston_Brooks <pbrooks193@gmail.com>
2025-12-17 17:01:16 +1000

    stuffing around with the graphics for the spell menu

--- games/game0/maps/map0/widgets/spellmenu.h
+++ games/game0/maps/map0/widgets/spellmenu.h
@@ -44,154 +44,154 @@ bbFlag SpellMenu_Constructor (bbWidget** self, void* Graphics,
     bbScreenPoints SP;
 
     SP.x = screen_coords.x + 11*8 + 35*8*0;
-    SP.y = screen_coords.y + 11*8 + 35*8*0;
+    SP.y = screen_coords.y + 80*8 + 35*8*0;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT1");
 
     SP.x = screen_coords.x + 11*8 + 35*8*1;
-    SP.y = screen_coords.y + 11*8 + 35*8*0;
+    SP.y = screen_coords.y + 80*8 + 35*8*0;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT2");
 
     SP.x = screen_coords.x + 11*8 + 35*8*2;
-    SP.y = screen_coords.y + 11*8 + 35*8*0;
+    SP.y = screen_coords.y + 80*8 + 35*8*0;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT3");
 
     SP.x = screen_coords.x + 11*8 + 35*8*3;
-    SP.y = screen_coords.y + 11*8 + 35*8*0;
+    SP.y = screen_coords.y + 80*8 + 35*8*0;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT4");
 
     SP.x = screen_coords.x + 11*8 + 35*8*4;
-    SP.y = screen_coords.y + 11*8 + 35*8*0;
+    SP.y = screen_coords.y + 80*8 + 35*8*0;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT5");
 
     SP.x = screen_coords.x + 11*8 + 35*8*5;
-    SP.y = screen_coords.y + 11*8 + 35*8*0;
+    SP.y = screen_coords.y + 80*8 + 35*8*0;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT6");
 
     SP.x = screen_coords.x + 11*8 + 35*8*6;
-    SP.y = screen_coords.y + 11*8 + 35*8*0;
+    SP.y = screen_coords.y + 80*8 + 35*8*0;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT7");
 
     SP.x = screen_coords.x + 11*8 + 35*8*7;
-    SP.y = screen_coords.y + 11*8 + 35*8*0;
+    SP.y = screen_coords.y + 80*8 + 35*8*0;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT8");
 
     SP.x = screen_coords.x + 11*8 + 35*8*8;
-    SP.y = screen_coords.y + 11*8 + 35*8*0;
+    SP.y = screen_coords.y + 80*8 + 35*8*0;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT9");
 
     SP.x = screen_coords.x + 11*8 + 35*8*9;
-    SP.y = screen_coords.y + 11*8 + 35*8*0;
+    SP.y = screen_coords.y + 80*8 + 35*8*0;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT10");
 
 
     SP.x = screen_coords.x + 11*8 + 35*8*0;
-    SP.y = screen_coords.y + 11*8 + 35*8*1;
+    SP.y = screen_coords.y + 80*8 + 35*8*1;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT11");
 
     SP.x = screen_coords.x + 11*8 + 35*8*1;
-    SP.y = screen_coords.y + 11*8 + 35*8*1;
+    SP.y = screen_coords.y + 80*8 + 35*8*1;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT12");
 
     SP.x = screen_coords.x + 11*8 + 35*8*2;
-    SP.y = screen_coords.y + 11*8 + 35*8*1;
+    SP.y = screen_coords.y + 80*8 + 35*8*1;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT13");
 
     SP.x = screen_coords.x + 11*8 + 35*8*3;
-    SP.y = screen_coords.y + 11*8 + 35*8*1;
+    SP.y = screen_coords.y + 80*8 + 35*8*1;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT14");
 
     SP.x = screen_coords.x + 11*8 + 35*8*4;
-    SP.y = screen_coords.y + 11*8 + 35*8*1;
+    SP.y = screen_coords.y + 80*8 + 35*8*1;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT15");
 
     SP.x = screen_coords.x + 11*8 + 35*8*5;
-    SP.y = screen_coords.y + 11*8 + 35*8*1;
+    SP.y = screen_coords.y + 80*8 + 35*8*1;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT16");
 
     SP.x = screen_coords.x + 11*8 + 35*8*6;
-    SP.y = screen_coords.y + 11*8 + 35*8*1;
+    SP.y = screen_coords.y + 80*8 + 35*8*1;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT17");
 
     SP.x = screen_coords.x + 11*8 + 35*8*7;
-    SP.y = screen_coords.y + 11*8 + 35*8*1;
+    SP.y = screen_coords.y + 80*8 + 35*8*1;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT18");
 
     SP.x = screen_coords.x + 11*8 + 35*8*8;
-    SP.y = screen_coords.y + 11*8 + 35*8*1;
+    SP.y = screen_coords.y + 80*8 + 35*8*1;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT19");
 
     SP.x = screen_coords.x + 11*8 + 35*8*9;
-    SP.y = screen_coords.y + 11*8 + 35*8*1;
+    SP.y = screen_coords.y + 80*8 + 35*8*1;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT20");
 
 
     SP.x = screen_coords.x + 11*8 + 35*8*0;
-    SP.y = screen_coords.y + 11*8 + 35*8*2;
+    SP.y = screen_coords.y + 80*8 + 35*8*2;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT21");
 
     SP.x = screen_coords.x + 11*8 + 35*8*1;
-    SP.y = screen_coords.y + 11*8 + 35*8*2;
+    SP.y = screen_coords.y + 80*8 + 35*8*2;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT22");
 
     SP.x = screen_coords.x + 11*8 + 35*8*2;
-    SP.y = screen_coords.y + 11*8 + 35*8*2;
+    SP.y = screen_coords.y + 80*8 + 35*8*2;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT23");
 
     SP.x = screen_coords.x + 11*8 + 35*8*3;
-    SP.y = screen_coords.y + 11*8 + 35*8*2;
+    SP.y = screen_coords.y + 80*8 + 35*8*2;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT24");
 
     SP.x = screen_coords.x + 11*8 + 35*8*4;
-    SP.y = screen_coords.y + 11*8 + 35*8*2;
+    SP.y = screen_coords.y + 80*8 + 35*8*2;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT25");
 
     SP.x = screen_coords.x + 11*8 + 35*8*5;
-    SP.y = screen_coords.y + 11*8 + 35*8*2;
+    SP.y = screen_coords.y + 80*8 + 35*8*2;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT26");
 
     SP.x = screen_coords.x + 11*8 + 35*8*6;
-    SP.y = screen_coords.y + 11*8 + 35*8*2;
+    SP.y = screen_coords.y + 80*8 + 35*8*2;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT27");
 
     SP.x = screen_coords.x + 11*8 + 35*8*7;
-    SP.y = screen_coords.y + 11*8 + 35*8*2;
+    SP.y = screen_coords.y + 80*8 + 35*8*2;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT28");
 
     SP.x = screen_coords.x + 11*8 + 35*8*8;
-    SP.y = screen_coords.y + 11*8 + 35*8*2;
+    SP.y = screen_coords.y + 80*8 + 35*8*2;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT29");
 
     SP.x = screen_coords.x + 11*8 + 35*8*9;
-    SP.y = screen_coords.y + 11*8 + 35*8*2;
+    SP.y = screen_coords.y + 80*8 + 35*8*2;
     bbWidget_constructor(&widget0, widgets, graphics,
                          SP, widget, "SPELLSELECT30");
 
