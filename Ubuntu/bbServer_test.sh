4d9b6b62428326f5cef1b6a91eacaddde9b29707
boston-brooks <pbrooks193@gmail.com>
2025-12-26 10:13:49 +1000

    working on what will be bbNetwork.c

--- Ubuntu/bbServer_test.sh
+++ Ubuntu/bbServer_test.sh
@@ -8,11 +8,14 @@ Color_Off='\033[0m'
 
  echo -e "Look, a Tortoise:\n${BGreen}\n       _____     ____\n     /      \  |  o |\n    |        |/ ___\| \n    |_________/     \n    |_|_| |_|_|\n//////////////////////////////////////////////////////////////${Color_Off}\n\nAttempting to compile:\n"
 
-echo "Opening Port 1701"
-sudo ufw allow 1701
 
 if gcc   -fsanitize=address -fno-omit-frame-pointer -g -I ../\
-       ../engine/echo_server/bbServer.c\
+       ../test/bbServer_test.c\
+       ../engine/logic/bbLeanPool.c\
+       ../engine/logic/bbBloatedPool.c\
+       ../engine/logic/bbVPool.c\
+       ../engine/threadsafe/bbThreadedPool.c\
+       ../engine/threadsafe/bbThreadedQueue.c\
        \
   -lcsfml-system -lcsfml-graphics -lcsfml-window -lcsfml-network -lm -w\
   -o ../games/game0/server.out;
--- engine/echo_server/bbServer.c
+++ engine/echo_server/bbServer.c
@@ -1,10 +1,16 @@
+//Copied form https://github.com/BostonBrooks/chatroom-in-c
+//Which was translated from https://github.com/orichalcink/chatroom
+
 #include <stdio.h>
-#include "engine/includes/CSFML.h"
-#include "engine/logic/bbTerminal.h"
-#include "engine/logic/bbIntTypes.h"
-int main(void)
-{
-    printf("hello server!\n");
+#include <SFML/System.h>
+#include <SFML/Graphics.h>
+#include <SFML/Window.h>
+#include <SFML/Network.h>
+
+#include "io.h"
+
+int main(void){
+    printf("Hello, server!\n");
 
     sfIpAddress localAddress;
     localAddress = sfIpAddress_getLocalAddress();
@@ -17,37 +23,124 @@ int main(void)
     char publicAddressString[64];
     sfIpAddress_toString(publicAddress, publicAddressString);
 
-    bbPrintf("Local address = %s\nPublic address = %s\nLoopback address = 127.0.0.1\n",
-       localAddressString, publicAddressString);
 
-    I32 port = 1701;
+    printf("Loopback address = 127.0.0.1\nLocal address = %s\nPublic address = %s\n",
+        localAddressString, publicAddressString);
 
-    bbPrintf("Listening on port %d\n", port);
+    int port = get_integer_input("Input server port: ");
 
     sfTcpListener* listener = sfTcpListener_create();
     sfSocketStatus status = sfTcpListener_listen(listener,port,sfIpAddress_Any);
-
+    //sfSocketStatus_print(status);
     if (status != sfSocketDone) {
-        bbPrintf("Could not connect to port 1701\n");
+        printf("Could not connect to the given port!");
         exit(EXIT_FAILURE);
     }
+    sfTcpSocket* sockets[8];
+    for (int i = 0; i < 8; ++i) {
+        sockets[i] = NULL;
+    }
 
-    bbPrintf("Waiting for clients to connect...\n");
+
+    printf("Waiting for clients to connect...\n");
 
     sfSocketSelector* selector = sfSocketSelector_create();
     sfSocketSelector_addTcpListener(selector, listener);
 
-    while (1)
-    {
+    while(1){
         sfBool flag = sfFalse;
-        I32 i = 1;
-        while (flag == sfFalse)
-        {
+        while (flag == sfFalse) {
             flag = sfSocketSelector_wait(selector, sfSeconds(10));
-            bbClr_line(1);
-            bbPrintf("Waiting for clients to connect...(%d0 seconds)\n", i++);
+
+
         }
+
+        if (sfSocketSelector_isTcpListenerReady(selector,listener) == sfTrue) {
+            sfTcpSocket *socket = NULL;
+            //socket = sfTcpSocket_create();
+            status = sfTcpListener_accept(listener, &socket);
+            //sfSocketStatus_print(status);
+            if (status != sfSocketDone) {
+                printf("Could not accept socket\n");
+            } else {}
+
+            int i = 0;
+            while (sockets[i] != NULL) {
+                i++;
+                //bbDebug("i = %d\n", i)
+            }
+            if (i < 8) {
+
+                sockets[i] = socket;
+                sfSocketSelector_addTcpSocket(selector, socket);
+            } else {
+
+                sfTcpSocket_destroy(socket);
+                printf("Socket array full\n");
+            }
+
+        }
+
+
+
+        sfPacket* packet;
+        packet = sfPacket_create();
+        for(int i = 0; i < 8;i++){
+            if (sockets[i] == NULL) continue;
+
+            if(sfSocketSelector_isTcpSocketReady(selector, sockets[i]) ==
+            sfTrue){
+
+                sfPacket* packet;
+                packet = sfPacket_create();
+                status = sfTcpSocket_receivePacket(sockets[i], packet);
+                if (status != sfSocketDone){
+                    sfTcpSocket_destroy(sockets[i]);
+                    sockets[i] = NULL;
+                    continue;
+                }
+
+                char data[512];
+
+                sfPacket_readString(packet,data);
+                printf("message: %s\n", data);
+
+                //Send out incoming packet
+                //sfPacket* echoPacket;
+                //echoPacket = sfPacket_create();
+                //sfPacket_writeString(echoPacket,data);
+
+                for (int j = 0; j < 8; j++){
+                    //send to self
+                    //if (j == i) continue;
+                    if(sockets[j] == NULL) continue;
+                    status = sfTcpSocket_sendPacket(sockets[j], packet);
+
+                    if (status != sfSocketDone){
+                        printf("Failed to echo user %d's message!\n", j);
+                    }
+                }
+                sfPacket_clear(packet);
+
+                continue;
+            }
+
+        }
+
+
+
     }
-    bbPrintf("We made it to the end!\n");
-    return 0;
+
+    printf("Server disconnected.\n");
+
+    for (int i = 0; i < 8; i++){
+        if(sockets[i]== NULL) continue;
+
+        sfTcpSocket_disconnect(sockets[i]);
+    }
+
+    exit(EXIT_SUCCESS);
+
+
+
 }
