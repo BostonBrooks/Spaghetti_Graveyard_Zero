166344d5de49c7e8eb98f41c22da1a8bf12e4e7a
boston-brooks <pbrooks193@gmail.com>
2025-12-28 09:32:49 +1000

    reading network packets

--- engine/network/bbNetwork_packet.h
+++ engine/network/bbNetwork_packet.h
@@ -1,20 +1,36 @@
 #ifndef BB_NETWORK_PACKET_H
 #define BB_NETWORK_PACKET_H
 
+#include <SFML/Network.h>
+
 #include "engine/logic/bbFlag.h"
 #include "engine/logic/bbPoolHandle.h"
+#include "engine/network/bbNetworkTime.h"
+
+#define PACKETTYPE_STRING            0
+#define PACKETTYPE_TIMESTAMP         1
+#define PACKETTYPE_REQUESTTIMESTAMP  2
+
+
+typedef union
+{
+    bbNetwork_timestamp timestamp;
+    char str[64];
+} bbNetwork_packet_data;
+
+
 
 typedef struct
 {
-
-    char data[64];
+    I32 type;
+    bbNetwork_packet_data data;
     bbPool_ListElement listElement;
 
 } bbNetwork_packet;
 
 
-bbFlag bbNetwork_packet_toStruct (sfPacket* packet, void* Struct);
-bbFlag bbNetwork_struct_toPacket (void* Struct, sfPacket* packet);
+bbFlag bbNetwork_packet_toStruct (sfPacket* packet, bbNetwork_packet* Struct);
+bbFlag bbNetwork_struct_toPacket (sfPacket* packet, bbNetwork_packet* Struct);
 
 
 
