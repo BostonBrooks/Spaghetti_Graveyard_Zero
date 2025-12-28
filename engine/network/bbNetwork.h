c312f3a009d2ade1f0e07b19970fc37822f65a59
boston-brooks <pbrooks193@gmail.com>
2025-12-28 15:59:10 +1000

    have started again with Network. Network must be launched asynchronously

--- engine/network/bbNetwork.h
+++ engine/network/bbNetwork.h
@@ -1,25 +1,18 @@
-//Translated into a module from bbNetwork_test.c, which is somewhat ad-hoc
-#ifndef BB_NETWORK_H
-#define BB_NETWORK_H
+///I'm starting things over, network connection must not block spawning thread
 
-#include <pthread.h>
 #include <stdatomic.h>
+#include <SFML/Network.h>
 
-#include "engine/includes/CSFML.h"
-
-#include "engine/network/bbNetwork_packet.h"
 #include "engine/threadsafe/bbThreadedQueue.h"
 
 
-
-
-//Defined outside
 typedef bbFlag bbNetwork_ProcessPacket (void* network, void* args);
 typedef bbFlag bbNetwork_PacketToStruct (sfPacket* packet, void* Struct);
 typedef bbFlag bbNetwork_StructToPacket (void* Struct, sfPacket* packet);
+typedef bbFlag bbNetwork_onConnect (void* network);
+typedef bbFlag bbNetwork_onDisconnect (void* network);
 
-
-typedef struct
+typedef struct bbNetwork
 {
     sfIpAddress address;
     I32 port;
@@ -33,21 +26,46 @@ typedef struct
     pthread_t send_thread;
     bbThreadedQueue outbox;
 
-    // behavior is defined outside this module
-    bbNetwork_ProcessPacket* process_packet;
+    // behaviors are defined outside this module:
+
     bbNetwork_PacketToStruct* packet_to_struct;
     bbNetwork_StructToPacket* struct_to_packet;
+    //notify front end
+    bbNetwork_onConnect* on_connect;
+    //notify front end
+    bbNetwork_onDisconnect* on_disconnect;
 
+//if true,
     atomic_bool quit;
+
+    //Does the server belong to this computer?
+    bool isLocal;
 } bbNetwork;
 
-bbFlag bbNetwork_init(bbNetwork* network, sfIpAddress address, I32 port,
-    bbNetwork_ProcessPacket* process_packet, bbNetwork_PacketToStruct* packet_to_struct,bbNetwork_StructToPacket* struct_to_packet);
-bbFlag bbNetwork_destroy(bbNetwork* network);
-void* bbNetwork_receiveThread(void* args);
-void* bbNetwork_sendThread(void* args);
-bbFlag bbNetwork_checkInbox(bbNetwork* network);
-bbFlag bbNetwork_packetToStruct(bbNetwork network, sfPacket* packet, void* Struct);
-bbFlag bbNetwork_structToPacket(bbNetwork network, sfPacket* packet, void* Struct);
 
-#endif //BB_NETWORK_H
+///create a new network object
+bbFlag bbNetwork_new(bbNetwork** network,
+    bbNetwork_ProcessPacket* process_packet,
+    bbNetwork_PacketToStruct* packet_to_struct,
+    bbNetwork_StructToPacket* struct_to_packet,
+    bbNetwork_onConnect* on_connect,
+    bbNetwork_onDisconnect* on_disconnect);
+
+///Asynchronously try to connect to a server, if successful, call bbNetwork_onConnect, otherwise call bbNetwork_onDisconnect
+///Launch bbNetwork_receiveThread
+///Then wait on outbox to send packets
+bbFlag bbNetwork_connect(bbNetwork* network, sfIpAddress address, I32 port);
+
+///called by bbNetwork_connect to asynchronously connect
+void* bbNetwork_spawn(void* args);
+///call bbNetwork_onDisconnect and end connection
+bbFlag bbNetwork_disconnect(bbNetwork* network);
+
+//one of the following is spawned as a new thread, and the other is called like a normal function
+/// this thread waits on inbox and sends packets over network
+void* bbNetwork_sendThread(void* args);
+
+/// this thread waits for incoming packets then outputs to inbox
+void* bbNetwork_receiveThread(void* args);
+
+//Functions to send to outbox or receive to inbox are called by main
