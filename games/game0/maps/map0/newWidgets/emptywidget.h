316c488affe36ad17c8e7679fcbb9136274964af
boston-brooks <pbrooks193@gmail.com>
2026-01-19 18:28:36 +1000

    working on new widget constructors and tree structure

---
+++ games/game0/maps/map0/newWidgets/emptywidget.h
@@ -0,0 +1,5 @@
+#include "engine/logic/bbFlag.h"
+#include "engine/userinterface/bbWidget.h"
+bbFlag bbWidget_constructor2(bbWidget** self, char* type, char* parent, char* name, I32 position_x, I32 position_y);
+
+
